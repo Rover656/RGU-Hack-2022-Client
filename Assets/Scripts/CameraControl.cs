1928687151a314e10a93d6db19dde32df9dcc3d7
Jamie <41928561+TheWitchDoctor13@users.noreply.github.com>
2022-03-26 23:20:01 +0000

    Camera Controls
    The silly camera controls, they work to a degree but will break when actually put to use

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float increase = 30f;
+    void Start()
+    { 
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        float angle = 0.0f;
+        float anglePitch = 0.0f;
+        
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            angle += increase;
+            Debug.Log("Help");
+        } else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            angle -= increase;
+            Debug.Log("Help 2 Electric boogaloo");
+        }
+        
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            anglePitch -= increase;
+            Debug.Log("Help but up");
+        } else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            anglePitch += increase;
+            Debug.Log("Help but down");
+        }
+
+        transform.RotateAround(Vector3.zero, Vector3.up, angle * Time.deltaTime);
+
+        transform.Rotate(Vector3.right, anglePitch * Time.deltaTime);
+    }
+}
